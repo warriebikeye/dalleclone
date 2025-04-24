0d2c93d5c3c37d9ffb48a10e1eb5eb19aa731cb7
warri ebikeye <ware_ebi@yahoo.com>
2025-04-24 16:35:43 +0100

    base-fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: '0.0.0.0', // Expose server to the network
     port: process.env.PORT || 4173, // Use the PORT environment variable or default to 4173
