5fae8fa0947f982bfe55c19954b89498fd874c3c
warri ebikeye <ware_ebi@yahoo.com>
2025-04-24 16:18:54 +0100

    reviewlink commkit

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
     host: '0.0.0.0', // Expose server to the network
     port: process.env.PORT || 4173, // Use the PORT environment variable or default to 4173
   },
+  preview: {
+    allowedHosts: ['dalle-frontend-a66r.onrender.com'], // Add your Render frontend domain here
+  },
 })
