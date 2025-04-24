495c305cf9ac645706c80fe14dc4f76dcff01d5b
warri ebikeye <ware_ebi@yahoo.com>
2025-04-24 16:07:06 +0100

    frotend port fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose server to the network
+    port: process.env.PORT || 4173, // Use the PORT environment variable or default to 4173
+  },
 })
