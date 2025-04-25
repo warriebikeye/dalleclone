3e1ba3002dce88074f4178f069649baf8278e4c3
Warri Ebikeye Cyprian <153061110+warriebikeye@users.noreply.github.com>
2025-04-25 06:46:20 +0100

    Update Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ _id, name, prompt, photo}) => {
     <div className='rounded-xl group relative shadow-card
     hover:shadow-cardhover card'>
       <img 
-      className='w-full h-auto object-cover rounded-xl'
+      className='w-full h-full absolute inset-0 object-cover rounded-xl'
       src={photo}
       alt={prompt}
       />
