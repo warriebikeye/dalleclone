d47077e5d40225f763508d3c6b4d49b14c6f97ac
warri ebikeye <ware_ebi@yahoo.com>
2025-04-27 17:55:41 +0100

    logox-fix

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,10 +2,12 @@ import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
 import deleteicon from './deleteicon.jpg';
+import logox from './logox.png';
 
 export {
   download,
   logo,
   preview,
   deleteicon,
+  logox,
 };
