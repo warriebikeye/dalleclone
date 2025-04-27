b53c3a894f065e682b807f8843274e66dd6787de
Warri Ebikeye Cyprian <153061110+warriebikeye@users.noreply.github.com>
2025-04-27 19:07:49 +0100

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -118,7 +118,7 @@ setForm({ ...form, prompt: randonPrompt})
                 <img
                 src={preview}
                 alt="preview"
-                className='w-9/12 h-9/12 object-contain opacity-40'/>
+                className='w-full h-full object-contain opacity-40'/>
               )
             }
             {generatingImg && (
