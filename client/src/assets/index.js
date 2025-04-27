0cb28bafb092d1695bfa710179d7d2d518c66766
warri ebikeye <ware_ebi@yahoo.com>
2025-04-27 17:51:01 +0100

    refix

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import deleteicon from './deleteicon.jpg';
 
 export {
   download,
   logo,
   preview,
+  deleteicon,
 };
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,12 +1,10 @@
 import Card from "./Card";
 import FormField from "./FormField";
 import Loader from "./Loader";
-import deleteicon from '../assets'; 
 
 
 export{
     Card,
     FormField,
     Loader,
-    deleteicon,
 }
